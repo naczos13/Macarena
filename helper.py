5350e61d2b4215b260aa91ac38d1738f9bdac342
naczos13 <naczos13@gmail.com>
2023-05-18 12:10:25 +0200

    Added the regneration of the simon instructions

--- helper.py
+++ helper.py
@@ -23,6 +23,7 @@ class VideoStreamWidget(object):
         self.mp_pose = mp.solutions.pose.PoseLandmark
         self.action_hand = self.mp_pose.LEFT_INDEX
         self.base_part = self.mp_pose.NOSE
+        self.action_radius = 20
 
     def update(self):
         self.capture = cv2.VideoCapture(0)
@@ -31,40 +32,42 @@ class VideoStreamWidget(object):
             if self.capture.isOpened():
                 (self.status, self.raw_frame) = self.capture.read()
                 self.find_human_pose_in_image()
-            time.sleep(.01)
-    
+            time.sleep(0.01)
+
     def show_frame(self):
         # as imput is raw_frame
         try:
             self.frame = self.raw_frame.copy()
         except AttributeError:
             pass
-        
+
         # check the landmarks
         try:
-            #self.pose_detection_results = self.raw_pose_detection_results.copy()
+            # self.pose_detection_results = self.raw_pose_detection_results.copy()
             self.landmarks = self.pose_detection_results.pose_landmarks.landmark
             self.draw_action_hand()
             self.draw_base_body_part()
+            if self.done_what_simon_said():
+                self.regenerate_simon_instruction()
         except (KeyError, AttributeError):
             pass
-        
+
         # Display frames in main program
         cv2.imshow(self.PROGRAM_NAME, self.frame)
         key = cv2.waitKey(1)
-        if key == ord('q'):
+        if key == ord("q"):
             self.capture.release()
             cv2.destroyAllWindows()
             exit(0)
-    
-    def show_menu(self):    
+
+    def show_menu(self):
         cv2.namedWindow(self.PROGRAM_NAME)
         self.frame = cv2.imread(self.FIRST_FRAME_PATH)
         if self.frame is not None:
             cv2.imshow(self.PROGRAM_NAME, self.frame)
         else:
             exit(1)
-            
+
     def find_human_pose_in_image(self):
         # Recolor image because mediapipe need RGB,
         # and cv2 has default BGR
@@ -74,7 +77,7 @@ class VideoStreamWidget(object):
 
         # Make detection
         self.pose_detection_results = self.pose_detector.process(image)
-        
+
         # # Memory optimization
         # self.frame.flags.writeable = True
         # # Recolor image back
@@ -83,8 +86,10 @@ class VideoStreamWidget(object):
     def draw_the_body_landmarks(self):
         try:
             mp.solutions.drawing_utils.draw_landmarks(
-                    self.frame, self.pose_detection_results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS
-                )
+                self.frame,
+                self.pose_detection_results.pose_landmarks,
+                mp.solutions.pose.POSE_CONNECTIONS,
+            )
         except Exception:
             sys.exit("Error in draw_the_body_landmarks probably run before detection")
 
@@ -94,10 +99,43 @@ class VideoStreamWidget(object):
         y = round(body_part.y * self.frame.shape[0])
         return x, y
 
-    def draw_action_hand(self):     
+    def draw_action_hand(self):
         x, y = self.get_body_part_window_coordinate(self.action_hand)
-        cv2.circle(self.frame, (x, y), 20, (0, 255, 0), -1)
-     
+        cv2.circle(self.frame, (x, y), self.action_radius, (0, 255, 0), -1)
+
     def draw_base_body_part(self):
         x, y = self.get_body_part_window_coordinate(self.base_part)
-        cv2.circle(self.frame, (x, y), 20, (255, 0, 0), -1)
+        cv2.circle(self.frame, (x, y), self.action_radius, (255, 0, 0), -1)
+        
+    def done_what_simon_said(self):
+        action_x, action_y = self.get_body_part_window_coordinate(self.action_hand)
+        base_x, base_y = self.get_body_part_window_coordinate(self.base_part)
+        
+        distance = math.dist(
+            [action_x, action_y],
+            [base_x, base_y])
+        
+        return distance < self.action_radius
+        
+        
+    def regenerate_simon_instruction(self):
+        self.action_hand = random.choice([
+            self.mp_pose.LEFT_INDEX,
+            self.mp_pose.RIGHT_INDEX
+        ])
+        
+        if self.action_hand == self.mp_pose.LEFT_INDEX:
+            self.base_part = random.choice([
+                self.mp_pose.NOSE,
+                self.mp_pose.RIGHT_SHOULDER,
+                self.mp_pose.RIGHT_INDEX,
+                self.mp_pose.RIGHT_ELBOW
+            ])
+        elif self.action_hand == self.mp_pose.RIGHT_INDEX:
+            self.base_part = random.choice([
+                self.mp_pose.NOSE,
+                self.mp_pose.LEFT_SHOULDER,
+                self.mp_pose.LEFT_INDEX,
+                self.mp_pose.LEFT_ELBOW
+            ])
+            
