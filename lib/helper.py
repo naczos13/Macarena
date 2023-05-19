6938dcfc9c6d805f1c9b34ae084c325015d22563
naczos13 <naczos13@gmail.com>
2023-05-19 14:01:08 +0200

    Add couple of functionalities
    - start event
    - right side menu
    - instruction
    - score system
    - save higest score to the file

--- lib/helper.py
+++ lib/helper.py
@@ -9,29 +9,31 @@ import sys
 
 
 class VideoStreamWidget(object):
-    def __init__(self):
+    def __init__(self, capure_input_from_camera=True):
         ### Constans
         ## Tune mediapipe detection and tracking
         self.POSE_DETECTOR = mp.solutions.pose.Pose(
             min_detection_confidence=0.5, min_tracking_confidence=0.5
         )
         self.PROGRAM_NAME = "Simon Says"
-        self.FIRST_FRAME_PATH = "first_frame.jpg"
+        self.FIRST_FRAME_PATH = "static/first_frame.jpg"
         self.ACTION_RADIUS = 20
         self.MP_POSE = mp.solutions.pose.PoseLandmark
+        self.PATH_TO_HIGH_SCORE = "static/high_score.txt"
         
-        # Start the thread to read frames from the video stream
-        self._thread = Thread(target=self.update, args=())
-        self._thread.daemon = True
-        self._thread.start()
+        if capure_input_from_camera:
+            # Start the thread to read frames from the video stream
+            self._thread = Thread(target=self.update, args=())
+            self._thread.daemon = True
+            self._thread.start()
 
-        self._action_hand = self.MP_POSE.LEFT_INDEX # TODO change to null, wait for user to start
-        self._base_part = self.MP_POSE.NOSE # TODO change to null, wait for user to start
-        self._highest_score = 0
+        self._action_hand, self._base_part = self.regenerate_simon_instruction()
+        self._highest_score = self.read_highest_score_from_file(self.PATH_TO_HIGH_SCORE)
         self._current_score = 0
         self._capture = None # Camera capture
         self._landmarks = None # Body parts position landmarks
-        self._left_menu = cv2.imread('menu_bar.jpg')    
+        self._raw_right_menu = cv2.imread('static/menu_bar.jpg')
+        self._games_runs = False    
 
     def update(self):
         self._capture = cv2.VideoCapture(0)
@@ -46,38 +48,120 @@ class VideoStreamWidget(object):
         # input is raw_frame
         try:
             frame = self._raw_frame.copy()
+            right_menu = self._raw_right_menu.copy()
         except AttributeError:
             pass
-
         # check the landmarks
         try:
-            self._landmarks = self._pose_detection_results.pose_landmarks.landmark
-            frame = self.draw_action_hand(camera_frame=frame, action_hand=self._action_hand, landmarks=self._landmarks)
-            frame = self.draw_base_body_part(camera_frame=frame, target_body_part=self._base_part, landmarks=self._landmarks)
-            if self.done_what_simon_said(active_hand=self._action_hand,
-                                         target_body_part=self._base_part,
-                                         landmarks=self._landmarks,
-                                         camera_frame=frame):
-                self._action_hand, self._base_part = self.regenerate_simon_instruction()
-            #self.draw_scores()
+            if self._games_runs:
+                self._landmarks = self._pose_detection_results.pose_landmarks.landmark
+                frame = self.draw_action_hand(camera_frame=frame, action_hand=self._action_hand, landmarks=self._landmarks)
+                frame = self.draw_base_body_part(camera_frame=frame, target_body_part=self._base_part, landmarks=self._landmarks)
+                if self.done_what_simon_said(active_hand=self._action_hand,
+                                            target_body_part=self._base_part,
+                                            landmarks=self._landmarks,
+                                            camera_frame=frame):
+                    self._action_hand, self._base_part = self.regenerate_simon_instruction()
+                    self._current_score += 1
+                    if self._current_score > self._highest_score:
+                        self._highest_score = self._current_score
         except (KeyError, AttributeError):
             pass
 
+        right_menu = self.draw_scores(right_bar=right_menu, max_score=self._highest_score, current_score=self._current_score)
+        right_menu = self.draw_instruction(right_bar=right_menu)
+        
+        
         # Display frames in main program
-        canvas = self.concate_camera_frame_with_menu(camera_frame=frame, right_bar=self._left_menu)
+        canvas = self.concate_camera_frame_with_menu(camera_frame=frame, right_bar=right_menu)
         cv2.imshow(self.PROGRAM_NAME, canvas)
         key = cv2.waitKey(1)
         if key == ord("q"):
-            self._capture.release()
-            cv2.destroyAllWindows()
-            exit(0)
+            self.exit_program()
+        if key == ord("s"):
+            self._games_runs = True
+    
+    def read_highest_score_from_file(self, path):
+        try:
+            with open(path, 'r') as file:
+                high_score = file.readline()
+        except FileNotFoundError:
+            return 0
+            
+        try:
+            high_score = int(high_score)
+            return high_score
+        except ValueError:
+            return 0
+    
+    def draw_instruction(self, right_bar):
+        font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        pos_x = 10  
+        pos_y = 150
+        color = (0, 0, 0) # black
+        
+        right_bar = cv2.putText(right_bar, "To score a point", (pos_x, pos_y), font, 
+                   1, color, 1, cv2.LINE_AA)
+        
+        pos_y = 170
+        right_bar = cv2.putText(right_bar, "connect", (pos_x, pos_y), font, 
+                   1, color, 1, cv2.LINE_AA)
+        
+        right_bar = cv2.circle(right_bar, (pos_x + (self.ACTION_RADIUS), pos_y + (2*self.ACTION_RADIUS)), self.ACTION_RADIUS, (0, 255, 0), -1)
+        
+        right_bar = cv2.putText(right_bar, "to", (pos_x + (2 * self.ACTION_RADIUS + 10), pos_y + (2*self.ACTION_RADIUS)), font, 
+                   1, color, 1, cv2.LINE_AA)
+        
+        right_bar = cv2.circle(right_bar, (pos_x + (5*self.ACTION_RADIUS), pos_y + (2*self.ACTION_RADIUS)), self.ACTION_RADIUS, (255, 0, 0), -1)
+        
+        return right_bar
+      
+    def draw_scores(self, right_bar, max_score, current_score):
+        font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        pos_x = 10  
+        pos_y = 50
+        color = (255, 0, 0) # blue
+        
+        str_current_score = f"Current Score: {current_score}"
+        right_bar = cv2.putText(right_bar, str_current_score, (pos_x, pos_y), font, 
+                   1, color, 2, cv2.LINE_AA)
+        
+        str_highest_score = f"Highest Score: {max_score}"
+        right_bar = cv2.putText(right_bar, str_highest_score, (pos_x, pos_y + 50), font, 
+                   1, color, 1, cv2.LINE_AA)
+        
+        return right_bar
+    
+    def exit_program(self):
+        self.safe_the_high_score(self.PATH_TO_HIGH_SCORE)
+        self._capture.release()
+        cv2.destroyAllWindows()
+        exit(0) 
+
+    def safe_the_high_score(self, path):
+        try:
+            with open(path, 'r') as reader:
+                high_score = reader.readline()
+        except FileNotFoundError:
+            return
+            
+        try:
+            high_score = int(high_score)
+            if self._highest_score > high_score:
+                try:
+                    with open(path, 'w') as writer:
+                        writer.write(str(self._highest_score))
+                except FileNotFoundError:
+                    return
+        except ValueError:
+            return
 
     def show_menu(self):
         cv2.namedWindow(self.PROGRAM_NAME)
         # read to self._raw_frame because this picture need to be display until the camera frame is ready
         self._raw_frame = cv2.imread(self.FIRST_FRAME_PATH)
-        if self._raw_frame is not None and self._left_menu is not None:
-            canvas = self.concate_camera_frame_with_menu(camera_frame=self._raw_frame, right_bar=self._left_menu)
+        if self._raw_frame is not None and self._raw_right_menu is not None:
+            canvas = self.concate_camera_frame_with_menu(camera_frame=self._raw_frame, right_bar=self._raw_right_menu)
             cv2.imshow(self.PROGRAM_NAME, canvas)
         else:
             exit(1)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import helper
+import lib.helper as helper
 
 
 def main():
